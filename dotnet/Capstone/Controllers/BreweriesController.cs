9dc46b712db07cf2fa2158ac975e51a34e0b2ce9
Jonnie Niesz <jonnieniesz@yahoo.com>
2021-04-05 15:21:17 -0400

    issues

---
+++ dotnet/Capstone/Controllers/BreweriesController.cs
@@ -0,0 +1,25 @@
+﻿using Capstone.DAO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class BreweriesController : ControllerBase
+    {
+        private readonly IBreweriesDAO breweriesDAO;
+
+        public BreweriesController(IBreweriesDAO _breweriesDAO)
+        {
+            breweriesDAO = _breweriesDAO;
+        }
+    }
+
+
+}
+
---
+++ dotnet/Capstone/Models/Brewery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Brewery
+    {
+        public int Brewery_id { get; set; }
+        public string Name { get; set; }
+        public string Phone_number { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string History { get; set; }
+        public bool Active { get; set; }
+        public string Brewery_img { get; set; }
+        public string Hours_of_operation { get; set; }
+        public int User_id { get; set; }
+    }
+}
+
