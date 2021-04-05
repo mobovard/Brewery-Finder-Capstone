4ab7c53e403aa50eac92bee56e4291b4aa670520
Christianna Kreimer <Christidawnk@gmail.com>
2021-04-05 15:53:06 -0400

    saving css updates

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -6,14 +6,13 @@
 </template>
 
 <script>
-import NavBar from './components/NavBar'
+import NavBar from "./components/NavBar";
 
 export default {
   components: { NavBar },
-}
+};
 </script>
 
 
 <style>
-
 </style>
--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
-  <b-navbar toggleable="md" class="frosty-primary" sticky>
-    <b-navbar-brand>Brewery Finder</b-navbar-brand>
+  <b-navbar toggleable="md" class="frosty-porter" sticky>
+    <b-navbar-brand
+      ><span class="text-belgian font-weight-bold"
+        >Brewery Finder</span
+      ></b-navbar-brand
+    >
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'home' }">Home</b-nav-item>
+        <b-nav-item :to="{ name: 'home' }"
+          ><span class="text-foam font-weight-bold">Home</span></b-nav-item
+        >
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
@@ -14,14 +20,14 @@
         <b-nav-item-dropdown id="user-dropdown" right>
           <!-- Using 'button-content' slot -->
           <template #button-content>
-            <BIconPersonCircle />
+            <BIconPersonCircle class="text-porter" />
           </template>
           <b-dropdown-item v-if="$store.getters.isLoggedIn">{{
             $store.state.user.username
           }}</b-dropdown-item>
           <b-dropdown-item
             :to="{ name: 'logout' }"
-            :disabled="!$store.state.token != ''"
+            :disabled="!$store.getters.isLoggedIn"
             >Sign Out</b-dropdown-item
           >
         </b-nav-item-dropdown>
