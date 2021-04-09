6fcde7a88b41807fe40414a701f5f45143745cc4
Chris Templeton <christemp188@gmail.com>
2021-04-09 12:56:49 -0400

    fixed css background class, added user id to brewery form and started work on submitting to api

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
-  
   <div class="frosty-porterdk text-foam mx-auto my-auto p-2 text-center">
-      <h1 class="text-wheat">Welcome to Brewery Finder</h1>
-      <h3 class="mt-3">Find your next brew here!</h3>
-      <p class="mt-3">
-        Sign-up to see local breweries, their history, and most importantly -
-        BEER SELECTION!
-      </p>
-    </div>
-  
+    <h1 class="text-wheat">Welcome to Brewery Finder</h1>
+    <h3 class="mt-3">Find your next brew here!</h3>
+    <p class="mt-3">
+      Sign-up to see local breweries, their history, and most importantly - BEER
+      SELECTION!
+    </p>
+    <button
+      class="btn bg-porter text-foam text-wheat-h"
+      v-if="!$store.getters.isLoggedIn"
+      @click="$router.push({ name: 'login' })"
+    >
+      Sign in
+    </button>
+    <button
+      class="btn bg-porter text-foam text-wheat-h ml-2"
+      v-if="!$store.getters.isLoggedIn"
+      @click="$router.push({ name: 'register' })"
+    >
+      Register
+    </button>
+  </div>
 </template>
 
 <script>
@@ -16,9 +28,7 @@ export default {
   components: {},
   name: "home",
   created() {
-    this.$store.commit('SET_BACKGROUND','./img/beer.jpg');
-  }
+    this.$store.commit("SET_BACKGROUND", "./img/beer.jpg");
+  },
 };
 </script>
-
-
