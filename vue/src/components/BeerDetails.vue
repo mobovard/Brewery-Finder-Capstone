caccae4cac331fe68749909b19fdf0c8adbd208b
Christianna Kreimer <Christidawnk@gmail.com>
2021-04-09 16:29:04 -0400

    created add review form. added it to the beer details page. need to link to DB and implement stars on beer details page

--- vue/src/components/BeerDetails.vue
+++ vue/src/components/BeerDetails.vue
@@ -12,41 +12,66 @@
     >
       <h1>{{ beer.name }}</h1>
       <h4>{{ $store.state.brewery.name }}</h4>
-
-      <div class="frosty-foam p-3 mt-2 rounded text-porter">
-        <div class="d-flex flex-column align-items-left">
-          <div class="flex-custom-img align-self-center">
-            <img :src="beer.image" class="rounded-circle img-fluid mb-3" />
+    </div>
+    <div class="frosty-foam p-3 mt-2 rounded text-porter">
+      <div class="d-flex flex-column align-items-left">
+        <div class="flex-custom-img align-self-center">
+          <img :src="beer.image" class="rounded-circle img-fluid mb-3" />
+        </div>
+        <div>
+          <div class="d-flex flex-row align-items-baseline">
+            <h3 class="text-belgian">{{ beer.beer_type }}</h3>
+            <h3 class="ml-4 mr-5 text-belgian">ABV: ( {{ beer.abv }}% )</h3>
           </div>
           <div>
-            <div class="d-flex flex-row align-items-baseline">
-              <h3 class="text-belgian">{{ beer.beer_type }}</h3>
-              <h3 class="ml-4 mr-5 text-belgian">ABV: ( {{ beer.abv }}% )</h3>
-            </div>
-            <div>
-              <p class="text-porter">
-                {{ beer.description }}
-              </p>
-            </div>
+            <p class="text-porter">
+              {{ beer.description }}
+            </p>
+            <p>
+              Favorite?
+              <input type="checkbox" />
+            </p>
+            <button
+              class="btn bg-porter text-foam text-wheat-h"
+              @click="displayAddReviewForm = !displayAddReviewForm"
+            >
+              Add a Review?
+            </button>
+            <b-row>
+              <b-col>
+                <AddReviewForm v-if="displayAddReviewForm" />
+              </b-col>
+            </b-row>
           </div>
-          <b-link
-            class="text-belgian text-fruit-h align-self-end"
-            @click="
-              $router.push({
-                name: 'brewery',
-                params: { id: $route.params.breweryId },
-              })
-            "
-            >Go Back: {{ $store.state.brewery.name }}
-          </b-link>
         </div>
+
+        <b-link
+          class="text-belgian text-fruit-h align-self-end"
+          @click="
+            $router.push({
+              name: 'brewery',
+              params: { id: $route.params.breweryId },
+            })
+          "
+          >Go Back: {{ $store.state.brewery.name }}
+        </b-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import AddReviewForm from "../components/AddReviewForm";
 export default {
+  components: {
+    AddReviewForm,
+  },
+  data() {
+    return { displayAddReviewForm: false };
+  },
+
+  props: [],
+  methods: {},
   computed: {
     beer() {
       return this.$store.state.beers.filter(
@@ -67,9 +92,9 @@ export default {
 .bg-img {
   position: fixed;
   left: 0;
-  top:0;
-  height:100vh;
-  width:100vw;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
   object-fit: cover;
   z-index: 0;
 }
