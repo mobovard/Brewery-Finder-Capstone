6590648a392860bcc3391e4b1e0de269fa88aa97
Christianna Kreimer <Christidawnk@gmail.com>
2021-04-12 15:31:03 -0400

    working on review form and hops rating

--- vue/src/components/AddReviewForm.vue
+++ vue/src/components/AddReviewForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-form class="d-grid" @submit.prevent="addReview">
+  <b-form class="d-grid" @submit.prevent="addNewReview">
     <b-row>
       <b-col>
         <h4 class="text-foam text-center mt-2 mb-3">
@@ -19,17 +19,27 @@
         />
       </b-col>
     </b-row>
-        <b-row>
+    <b-row>
       <b-col>
         <label class="d-flex align-self-start" for="review">Review: </label>
-        <b-form-textarea class="mb-3" id="review" v-model="newReview.review"></b-form-textarea>
+        <b-form-textarea
+          class="mb-3"
+          id="review"
+          v-model="newReview.review"
+        ></b-form-textarea>
       </b-col>
     </b-row>
 
     <b-row>
       <b-col class="d-flex justify-content-start align-items-center">
         <label class="mr-2" for="rating">Rating:</label>
-        <b-form-select id="rating" v-model.number="newReview.rating" :select-size="1" size="sm" class="selectRating" > 
+        <b-form-select
+          id="rating"
+          v-model.number="newReview.rating"
+          :select-size="1"
+          size="sm"
+          class="selectRating"
+        >
           <option value="1">1 Hops</option>
           <option value="2">2 Hops</option>
           <option value="3">3 Hops</option>
@@ -38,24 +48,32 @@
         </b-form-select>
       </b-col>
     </b-row>
-
-   <!-- Buttons -->
+    <!-- Buttons -->
     <b-row>
       <b-col>
-        <button class="btn bg-porter text-foam text-wheat-h ml-2" v-on:click.prevent="resetForm" type="cancel">Cancel</button>
-        <button class="btn bg-porter text-foam text-wheat-h ml-2" type="submit">Submit</button>
+        <button
+          class="btn bg-porter text-foam text-wheat-h ml-2"
+          v-on:click.prevent="resetForm"
+          type="cancel"
+        >
+          Cancel
+        </button>
+        <button class="btn bg-porter text-foam text-wheat-h ml-2" type="submit">
+          Submit
+        </button>
       </b-col>
     </b-row>
   </b-form>
 </template>
 
 <script>
+import BreweriesService from "../services/BreweriesService";
 export default {
   name: "addReview",
   data() {
     return {
       newReview: {
-        reviewer: "",
+        reviewer: this.$store.state.user.name,
         title: "",
         rating: 0,
         review: "",
@@ -64,11 +82,13 @@ export default {
   },
   methods: {
     addNewReview() {
-      const productID = this.$route.params.id;
-      this.newReview.id = productID;
-      this.$store.commit("ADD_REVIEW", this.newReview);
-      this.$router.push({ name: "product-detail", params: { id: productID } });
-      // TODO: send the visitor back to the product page to see the new review
+      BreweriesService.addReview(this.newReview)
+        .then((resp) => {
+          this.$store.commit("SET_REVIEWS", resp.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     resetForm() {
       this.newReview = {};
@@ -78,7 +98,7 @@ export default {
 </script>
 
 <style scoped>
-.selectRating{
-    max-width: 4rem;
+.selectRating {
+  max-width: 4rem;
 }
 </style>
