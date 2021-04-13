70486d3a848b369a94b24950356df1f69f6bc253
maddy bovard <m.bovard55@gmail.com>
2021-04-13 15:01:39 -0400

    adding in reviews

--- vue/src/components/BeerReviews.vue
+++ vue/src/components/BeerReviews.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="rating d-grid">
-    <div v-for="review in reviews" v-bind:key="review.rating_id">
-      <b-row>
+    <div v-for="review in reviews" v-bind:key="review.rating_id" class="frosty-foam p-3 mt-2 rounded text-porter">
+      <b-row class="mx-0">
         <b-col-sm>
           <img
             src="../img/hops.png"
-            class="ratingHops"
+            class="ratingHops mr-1"
             v-bind:title="review.rating"
             v-for="n in review.rating"
             v-bind:key="n"
           />
         </b-col-sm>
         <b-col>
+          <h5>
           {{ review.title }}
+          </h5>
         </b-col>
       </b-row>
-      <b-row>{{ review.review }}</b-row>
+      <b-row class="mb-1 mx-0">{{ review.review }}</b-row>
+      <b-row class="d-flex justify-content-end mx-0">
+        <div>
+        - {{review.username}}
+        </div>
+      </b-row>
     </div>
   </div>
 </template>
@@ -43,6 +50,6 @@ export default {
 
 <style scoped>
 .ratingHops {
-  max-height: 1rem;
+  max-height: 1.25rem;
 }
 </style>
